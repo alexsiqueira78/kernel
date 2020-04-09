d4a077d3ab3c16adad9588c79b096dc13ab28b1f
Alex Siqueira <alexsiqueira.sp@gmail.com>
2020-04-09 08:33:38 -0300

    Updates

---
+++ precache-manifest.d45c859e6c3798ed2e19b2ba4effcf5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d07f56df039ba162bfac2b096adee4e",
+    "url": "/kernel/index.html"
+  },
+  {
+    "revision": "a852250f7c735039687f",
+    "url": "/kernel/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a232ab3d066f8dc77050",
+    "url": "/kernel/static/css/main.523bddd9.chunk.css"
+  },
+  {
+    "revision": "a852250f7c735039687f",
+    "url": "/kernel/static/js/2.fafe7f4b.chunk.js"
+  },
+  {
+    "revision": "aff540c091dde5668605e9abc9b5db02",
+    "url": "/kernel/static/js/2.fafe7f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a232ab3d066f8dc77050",
+    "url": "/kernel/static/js/main.1702f8c9.chunk.js"
+  },
+  {
+    "revision": "0c4c85b3ceb4bf43bee7",
+    "url": "/kernel/static/js/runtime-main.5b2271e5.js"
+  }
+]);
